4652f13326e3786ab094434312676eae2163bd9a
Normalverbraucher <diedreifragezeichen3@gmail.com>
2016-06-21 01:11:45 +0200

    Add files via upload

--- ofApp.cpp
+++ ofApp.cpp
@@ -18,22 +18,16 @@ void ofApp::setup() {
 	
 	post.init(ofGetWidth(), ofGetHeight());
 	post.createPass<BloomPass>()->setEnabled(true);
-	post.createPass<DofAltPass>()->setEnabled(false);
+	post.createPass<DofAltPass>()->setEnabled(true);
 	post.createPass<GodRaysPass>()->setEnabled(true);
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
 
-	for (int i = 0; i < NSTREETVIEW; i++) {
+	/*for (int i = 0; i < NSTREETVIEW; i++) {
 		streetview[i].update();
 	}
-
-	/*for (int i = 0; i < NSTREETVIEW; i++) {
-		for (int j = 0; j < NMESH; j++) {
-			mesh[j] = streetview[i].getDethMesh();
-		}
-	}*/
 
 	mesh[0] = streetview[0].getDethMesh();
 	mesh[1] = streetview[1].getDethMesh();
@@ -43,7 +37,7 @@ void ofApp::update() {
 
 	for (int i = 0; i < NMESH; i++) {
 		mesh[i].setMode(OF_PRIMITIVE_POINTS);
-	}
+	}*/
 }
 
 //--------------------------------------------------------------
@@ -52,6 +46,7 @@ void ofApp::draw() {
 	ofSetColor(ofColor::cyan);
 	
 	post.begin(cam);
+	//cam.begin();
 	
 	mesh[0].draw();
 	
@@ -79,6 +74,7 @@ void ofApp::draw() {
 	ofPopMatrix();
 	
 	post.end();
+	//cam.end();
 
 }
 
@@ -89,6 +85,50 @@ void ofApp::keyPressed(int key) {
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key) {
+
+	if (key == 'r') {
+		for (int i = 0; i < NSTREETVIEW; i++) {
+			streetview[i].update();
+		}
+		
+		mesh[0] = streetview[0].getDethMesh();
+		mesh[1] = streetview[1].getDethMesh();
+		mesh[2] = streetview[2].getDethMesh();
+		mesh[3] = streetview[3].getDethMesh();
+		mesh[4] = streetview[4].getDethMesh();
+
+		for (int i = 0; i < NMESH; i++) {
+			mesh[i].setMode(OF_PRIMITIVE_POINTS);
+		}
+
+		cout << mesh[0].getNumVertices() + mesh[1].getNumVertices() + mesh[2].getNumVertices() + mesh[3].getNumVertices() + mesh[4].getNumVertices()<< endl;
+
+	}
+	
+	else if (key == 'k') {
+		mesh[1].save("mesh1", false);
+		mesh[2].save("mesh2", false);
+		mesh[4].save("mesh4", false);
+	}
+
+	else if (key == 'l') {
+		
+		//mesh[0].load("kokura.ply");
+		mesh[0].load("mesh0.ply");
+		mesh[1].load("mesh1.ply");
+		mesh[2].load("mesh2.ply");
+		mesh[3].load("mesh3.ply");
+		mesh[4].load("mesh4.ply");
+		
+		mesh[0].setMode(OF_PRIMITIVE_POINTS);
+		mesh[1].setMode(OF_PRIMITIVE_POINTS);
+		mesh[2].setMode(OF_PRIMITIVE_POINTS);
+		mesh[3].setMode(OF_PRIMITIVE_POINTS);
+		mesh[4].setMode(OF_PRIMITIVE_POINTS);
+
+		cout << mesh[0].getNumVertices() + mesh[1].getNumVertices() + mesh[2].getNumVertices() + mesh[3].getNumVertices() + mesh[4].getNumVertices() << endl;
+
+	}
 
 }
 
@@ -103,8 +143,10 @@ void ofApp::mouseDragged(int x, int y, int button) {
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) {
-
+void ofApp::mousePressed(ofMouseEventArgs &mouse) {
+	{
+		cam.toggleControl();
+	}
 }
 
 //--------------------------------------------------------------
--- ofApp.h
+++ ofApp.h
@@ -3,8 +3,12 @@
 #include "ofMain.h"
 #include "ofxStreetView.h"
 #include "ofxPostProcessing.h"
+#include "ofxFirstPersonCamera.h"
+
 #define NSTREETVIEW 6
 #define NMESH 6
+
+#pragma once
 
 class ofApp : public ofBaseApp {
 public:
@@ -16,13 +20,14 @@ public:
 	void keyReleased(int key);
 	void mouseMoved(int x, int y);
 	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
+	void mousePressed(ofMouseEventArgs &mouse);
 	void mouseReleased(int x, int y, int button);
 	void windowResized(int w, int h);
 	void dragEvent(ofDragInfo dragInfo);
 	void gotMessage(ofMessage msg);
 
-	ofEasyCam cam;
+	//ofEasyCam cam;
+	ofxFirstPersonCamera cam;
 
 	ofxStreetView streetview[NSTREETVIEW];
 
